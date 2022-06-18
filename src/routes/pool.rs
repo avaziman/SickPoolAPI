f7f55562684f8e853d8c203fe970dfd8b5f71415
ItsSickGuy <81191073+ItsSickGuy@users.noreply.github.com>
2022-06-19 00:15:01 +0300

    try1

--- src/routes/pool.rs
+++ src/routes/pool.rs
@@ -148,20 +148,34 @@ pub struct SearchRes {
     pub results: Vec<HashMap<String, String>>,
     pub total: i64,
 }
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Block {
-    pub number: u32,
+#[derive(Debug)]
+pub struct BlockRaw {
+    pub reward: i64,
+    pub time: i64,
+    pub duration: i64,
     pub height: u32,
-    pub block_type: String,
-    pub is_accepted: bool,
-    pub duration: u64,
-    pub time: u64,
-    pub solver: String,
-    pub reward: u64,
+    pub number: u32,
     pub difficulty: f64,
     pub effort_percent: f64,
-    pub hash: String,
+    pub chain: [i8; 8],
+    pub solver: [i8; 16],
+    pub worker: [i8; 34],
+    pub hash: [i8; 64],
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Block {
+    pub reward: i64,
+    pub time: i64,
+    pub duration: i64,
+    pub height: u32,
+    pub number: u32,
+    pub difficulty: f64,
+    pub effort_percent: f64,
     pub chain: String,
+    pub solver: String,
+    pub worker: String,
+    pub hash: String,
 }
 
 #[derive(Debug, Serialize)]
@@ -174,16 +188,19 @@ pub struct BlockRes {
 async fn blocks(info: web::Query<BlocksQuery>, api_data: web::Data<SickApiData>) -> impl Responder {
     // let offset : u32 = info.page * info.limit as u32;
     //TODO: check validity of sortby and sortdir and chains
+
+    let mut sort_index = info.sortby.clone();
+    sort_index.insert_str(0, "block:");
+    println!("{}", sort_index);
+
     let mut con = api_data.redis.clone();
-    let res: BlockRes = redis::cmd("FT.SEARCH")
-        .arg("SP:block_index")
-        .arg("*")
-        .arg("LIMIT")
+    let res: BlockRes = redis::cmd("FCALL")
+        .arg("getblocksbyindex")
+        .arg(1)
+        .arg(sort_index)
         .arg(info.page * info.limit as u32) // offset
         .arg(info.limit) // num (limit)
-        .arg("SORTBY")
-        .arg(info.sortby.to_string())
-        .arg(info.sortdir.to_uppercase().to_string())
+        // .arg(info.sortdir.to_uppercase())
         .query_async(&mut con)
         .await
         .unwrap();
@@ -237,24 +254,25 @@ async fn dashboard(
     let addr: String = addr.into_inner();
 
     let is_id = addr.ends_with('@');
-    let filter : TsFilterOptions;
+    let filter: TsFilterOptions;
 
     if is_id {
-        filter =  TsFilterOptions::default()
-        .with_labels(true)
-        .equals("type", "hashrate")
-        .equals("identity", &addr);
+        filter = TsFilterOptions::default()
+            .with_labels(true)
+            .equals("type", "hashrate")
+            .equals("identity", &addr);
     } else {
-        filter =  TsFilterOptions::default()
-        .with_labels(true)
-        .equals("type", "hashrate")
-        .equals("address", &addr);
+        filter = TsFilterOptions::default()
+            .with_labels(true)
+            .equals("type", "hashrate")
+            .equals("address", &addr);
     }
 
-    let workers_hashrate: TsMrange<u64, f64> = match con.ts_mrange("0", "+", Some(0), None, filter).await {
-        Ok(r) => r,
-        Err(_) => return HttpResponse::InternalServerError().body("Data not found"),
-    };
+    let workers_hashrate: TsMrange<u64, f64> =
+        match con.ts_mrange("0", "+", Some(0), None, filter).await {
+            Ok(r) => r,
+            Err(_) => return HttpResponse::InternalServerError().body("Data not found"),
+        };
 
     let res = json!({ "hashrate": workers_hashrate.values[0].values});
 
@@ -273,138 +291,125 @@ pub fn pool_route(cfg: &mut web::ServiceConfig) {
     cfg.service(dashboard);
 }
 
-impl redis::FromRedisValue for SearchRes {
-    fn from_redis_value(value: &redis::Value) -> redis::RedisResult<Self> {
-        match value {
-            redis::Value::Bulk(items) => {
-                if items.len() <= 2 {
-                    Ok(SearchRes {
-                        results: vec![],
-                        total: 0,
-                    })
-                } else {
-                    let total_res_count: i64 =
-                        redis::from_redis_value(items.first().unwrap()).unwrap();
-
-                    let mut results: Vec<HashMap<String, String>> = Vec::new();
-                    for item in items.iter().skip(1) {
-                        match item {
-                            redis::Value::Bulk(props) => {
-                                let props_hm: HashMap<String, String> =
-                                    redis::from_redis_value(item).unwrap();
-                                results.push(props_hm);
-                                // println!("PROPS {:?}", props);
-                                // println!("PROPS_HM {:?}", props_hm);
-                            }
-                            _ => {} // (key name, we don't need it for now)
-                        }
-                    }
-
-                    Ok(SearchRes {
-                        results: results,
-                        total: total_res_count,
-                    })
-                }
-            }
-            _ => Err(redis::RedisError::from((
-                redis::ErrorKind::TypeError,
-                "Expected bulk result for FT.SEARCH command",
-            ))),
-        }
-    }
-}
-
 impl redis::FromRedisValue for BlockRes {
     fn from_redis_value(value: &redis::Value) -> redis::RedisResult<Self> {
-        let search_res: SearchRes = redis::from_redis_value(value).unwrap();
-
-        // println!("{:?}", search_res);
-        let mut blocks_vec: Vec<Block> = Vec::new();
-        // TODO: exception handle
-        for item in search_res.results {
-            let block = parse_block(item);
-            match block {
-                Ok(block) => blocks_vec.push(block),
-                Err(err) => eprintln!("Failed to parse block: {}", err),
-            }
+        let items: Vec<redis::Value> = redis::from_redis_value(value)?;
+        if items.len() < 2 {
+            return Ok(BlockRes {
+                blocks: vec![],
+                total: 0,
+            });
         }
+        let total_res_count: i64 = redis::from_redis_value(items.first().unwrap())?;
 
+        let block_arr: Vec<redis::Value> = redis::from_redis_value(&items[1])?;
+        let mut results: Vec<Block> = Vec::new();
+
+        for block in block_arr {
+            let bytes: Vec<u8> = redis::from_redis_value(&block)?;
+            println!("{:?}", bytes);
+            // unsafe {
+            //     let block = Block {
+            //         reward: std::mem::transmute_copy::<[u8;8], i64>(&bytes[0..8].try_into().unwrap()),
+            //         time: std::mem::transmute_copy::<[u8;8], i64>(&bytes[8..16].try_into().unwrap()),
+            //         duration: std::mem::transmute_copy::<[u8;8], i64>(&bytes[16..24].try_into().unwrap()),
+            //         height: std::mem::transmute_copy::<[u8;4], u32>(&bytes[24..28].try_into().unwrap()),
+            //         number: std::mem::transmute_copy::<[u8;4], u32>(&bytes[28..16].try_into().unwrap()),
+            //         difficulty: std::mem::transmute_copy::<[u8;8], i64>(&bytes[8..16].try_into().unwrap()),
+            //         effort_percent:std::mem::transmute_copy::<[u8;8], i64>(&bytes[8..16].try_into().unwrap()),
+            //         chain: String::from_utf8((&bytes[48..56]).to_vec())?,
+            //         solver: String::from_utf8((&bytes[56..90]).to_vec())?,
+            //         worker: String::from_utf8((&bytes[90..106]).to_vec())?,
+            //         hash: String::from_utf8((&bytes[106..170]).to_vec())?,
+            //     };
+            // println!("BLOCK {:?}", block);
+            // results.push(block);
+            // }
+        }
         Ok(BlockRes {
-            blocks: blocks_vec,
-            total: search_res.total,
+            blocks: results,
+            total: total_res_count,
         })
     }
 }
+
+// impl redis::FromRedisValue for BlockRes {
+//     fn from_redis_value(value: &redis::Value) -> redis::RedisResult<Self> {
+//         let search_res: SearchRes = redis::from_redis_value(value).unwrap();
+//         println!("{:?}", search_res);
+//         let mut blocks_vec: Vec<Block> = Vec::new();
+//         // TODO: exception handle
+//         for item in search_res.results {
+//             // let block = parse_block(item);
+//             // match block {
+//             //     Ok(block) => blocks_vec.push(block),
+//             //     Err(err) => eprintln!("Failed to parse block: {}", err),
+//             }
+//         }
+
+//         Ok(BlockRes {
+//             blocks: blocks_vec,
+//             total: search_res.total,
+//         })
+//     }
+// }
 
 // pub enum BlockStatus {
 //     Rejected = 0,
 //     Accepted = 1,
 // }
 
-pub fn parse_block(hm: HashMap<String, String>) -> Result<Block, String> {
-    let block = Block {
-        number: hm
-            .get("number")
-            .ok_or("No number key")?
-            .parse::<u32>()
-            .unwrap(),
-        height: hm
-            .get("height")
-            .ok_or("No height key")?
-            .parse::<u32>()
-            .unwrap(),
-        chain: hm.get("chain").ok_or("No chain key")?.to_string(),
-        block_type: hm.get("type").ok_or("No type key")?.to_string(),
-        is_accepted: if hm
-            .get("accepted")
-            .ok_or("No accepted key")?
-            .parse::<u8>()
-            .unwrap()
-            == 1
-        {
-            true
-        } else {
-            false
-        },
-        time: hm.get("time").ok_or("No time key")?.parse::<u64>().unwrap(),
-        duration: hm
-            .get("duration")
-            .ok_or("No duration key")?
-            .parse::<u64>()
-            .unwrap(),
-        solver: hm.get("solver").ok_or("No solver key")?.to_string(),
-        reward: hm
-            .get("reward")
-            .ok_or("No reward key")?
-            .parse::<u64>()
-            .unwrap(),
-        difficulty: hm
-            .get("difficulty")
-            .ok_or("No difficulty key")?
-            .parse::<f64>()
-            .unwrap(),
-        effort_percent: hm
-            .get("effort_percent")
-            .ok_or("No effort_percent key")?
-            .parse::<f64>()
-            .unwrap(),
-        hash: hm.get("hash").ok_or("No hash key")?.to_string(),
-    };
-
-    //         let block = Block {
-    //     number: item.get("number").ok_or()?.parse::<u32>().unwrap(),
-    //     height: item["height"].parse::<u32>().unwrap(),
-    //     chain: item["chain"].to_string(),
-    //     is_accepted: item["accepted"].parse::<bool>().unwrap(),
-    //     time: item["time"].parse::<u64>().unwrap(),
-    //     worker: item["worker"].to_string(),
-    //     reward: item["reward"].parse::<u64>().unwrap(),
-    //     difficulty: item["difficulty"].parse::<f64>().unwrap(),
-    //     effort_percent: item["effort_percent"].parse::<f64>().unwrap(),
-    //     hash: item["hash"].to_string(),
-    // };
-    Ok(block)
-}
+// pub fn parse_block(hm: HashMap<String, String>) -> Result<Block, String> {
+//     let block = Block {
+//         number: hm
+//             .get("number")
+//             .ok_or("No number key")?
+//             .parse::<u32>()
+//             .unwrap(),
+//         height: hm
+//             .get("height")
+//             .ok_or("No height key")?
+//             .parse::<u32>()
+//             .unwrap(),
+//         chain: hm.get("chain").ok_or("No chain key")?.to_string(),
+//         block_type: hm.get("type").ok_or("No type key")?.to_string(),
+//         is_accepted: if hm
+//             .get("accepted")
+//             .ok_or("No accepted key")?
+//             .parse::<u8>()
+//             .unwrap()
+//             == 1
+//         {
+//             true
+//         } else {
+//             false
+//         },
+//         time: hm.get("time").ok_or("No time key")?.parse::<u64>().unwrap(),
+//         duration: hm
+//             .get("duration")
+//             .ok_or("No duration key")?
+//             .parse::<u64>()
+//             .unwrap(),
+//         solver: hm.get("solver").ok_or("No solver key")?.to_string(),
+//         reward: hm
+//             .get("reward")
+//             .ok_or("No reward key")?
+//             .parse::<u64>()
+//             .unwrap(),
+//         difficulty: hm
+//             .get("difficulty")
+//             .ok_or("No difficulty key")?
+//             .parse::<f64>()
+//             .unwrap(),
+//         effort_percent: hm
+//             .get("effort_percent")
+//             .ok_or("No effort_percent key")?
+//             .parse::<f64>()
+//             .unwrap(),
+//         hash: hm.get("hash").ok_or("No hash key")?.to_string(),
+//     };
+//     Ok(block)
+// }
 
 // impl<T: redis::FromRedisValue> redis::FromRedisValue for FtQuery<T> {
 //     fn from_redis_value(v: &redis::Value) -> redis::RedisResult<Self> {
